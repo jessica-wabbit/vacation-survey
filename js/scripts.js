242dc76d1d8e5e32b71d5da1b9e458e3b1310d69
Jessica Robertson <jlynnr90@gmail.com>
2017-02-25 16:18:08 -0800

    Add two new questions to survey in html and adjust javascript to incorporate the changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business (or back-end) logic:
-var add = function(number1, number2, number3, number4) {
-  return number1 + number2 + number3 + number4;
+var add = function(answer1, answer2, answer3, answer4, answer5, answer6) {
+  return answer1 + answer2 + answer3 + answer4 + answer5 + answer6;
 };
 
 // Everything below this line is user interface (or front-end) logic:
@@ -8,6 +8,8 @@ $(document).ready(function() {
  $("form#quiz").submit(function(event) {
    event.preventDefault();
 
+   var language = parseInt($("input:radio[name=language]:checked").val());
+   var family = parseInt($("input:radio[name=family]:checked").val());
    var hiking = parseInt($("#hiking").val());
    var liketo = parseInt($("#liketo").val());
    var zipline = parseInt($("#zipline").val());
@@ -15,24 +17,24 @@ $(document).ready(function() {
 
 
 
-   var result = add(hiking, liketo, zipline, activity);
+   var result = add(language, family, hiking, liketo, zipline, activity);
 
-   if (result >= 4 && result <=7) {
+   if (result >= 6 && result <=9) {
      $("#hardcore").hide();
      $("#lightadventure").hide();
      $("#eurotour").hide();
      $("#caribbeancruise").show();
-   } else if (result >=8 && result <=11) {
+   } else if (result >=10 && result <=14) {
      $("#hardcore").hide();
      $("#lightadventure").hide();
      $("#caribbeancruise").hide();
      $("#eurotour").show();
-   } else if (result >=12 && result <=14) {
+   } else if (result >=15 && result <=19) {
      $("#hardcore").hide();
      $("#caribbeancruise").hide();
      $("#eurotour").hide();
      $("#lightadventure").show();
-   } else if (result === 15 || result === 16) {
+   } else if (result >=20 && result <=23) {
      $("#caribbeancruise").hide();
      $("#eurotour").hide();
      $("#lightadventure").hide();
